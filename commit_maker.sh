5b9543dea6346516b485b6b5f192606d73123887
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2023-02-14 16:15:00 +0200

    Do task No #(0*394592)

--- commit_maker.sh
+++ commit_maker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for i in {0..100..2}
+for i in {0..10..2}
 do
   touch algo${i}.py
   git add .
-  git commit -m "Do task No #{$i}394592"
+  git commit -m "Do task No #($i*394592)"
   rm algo${i}.py
   git add .
-  git commit -m "Do task No #{$i}569457"
+  git commit -m "Do task No #($i*569457)"
 done
