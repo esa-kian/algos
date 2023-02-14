0374d6312c4a765d28392406cb0acc4beab07c09
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2023-02-14 16:11:17 +0200

    Do task No #

--- commit_maker.sh
+++ commit_maker.sh
@@ -4,8 +4,8 @@ for i in {0..100..2}
 do
   touch algo${i}.py
   git add .
-  git commit -m "create Algo file No $i"
+  git commit -m "Do task No #$i394592"
   rm algo${i}.py
   git add .
-  git commit -m "delete Algo file No $i"
+  git commit -m "Do task No #$i569457"
 done
