21fc9be9758ad5b43332cc0b4910474aa642aec2
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2023-02-14 16:01:35 +0200

    create Algo file No 0

--- commit_maker.sh
+++ commit_maker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-for i in {0..10..2}
+for i in {0..100..2}
 do
+  touch algo${i}.py
+  git add .
+  git commit -m "create Algo file No $i"
   rm algo${i}.py
   git add .
   git commit -m "delete Algo file No $i"
