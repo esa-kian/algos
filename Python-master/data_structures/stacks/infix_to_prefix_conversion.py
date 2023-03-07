054b007282c8b7d40687b2a66efdf19444529ece
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2023-03-07 20:23:16 +0200

    Add new algorithms

---
+++ Python-master/data_structures/stacks/infix_to_prefix_conversion.py
@@ -0,0 +1,93 @@
+"""
+Output:
+
+Enter an Infix Equation = a + b ^c
+ Symbol  |  Stack  | Postfix
+----------------------------
+   c     |         | c
+   ^     | ^       | c
+   b     | ^       | cb
+   +     | +       | cb^
+   a     | +       | cb^a
+         |         | cb^a+
+
+         a+b^c (Infix) ->  +a^bc (Prefix)
+"""
+
+
+def infix_2_postfix(infix):
+    stack = []
+    post_fix = []
+    priority = {
+        "^": 3,
+        "*": 2,
+        "/": 2,
+        "%": 2,
+        "+": 1,
+        "-": 1,
+    }  # Priority of each operator
+    print_width = len(infix) if (len(infix) > 7) else 7
+
+    # Print table header for output
+    print(
+        "Symbol".center(8),
+        "Stack".center(print_width),
+        "Postfix".center(print_width),
+        sep=" | ",
+    )
+    print("-" * (print_width * 3 + 7))
+
+    for x in infix:
+        if x.isalpha() or x.isdigit():
+            post_fix.append(x)  # if x is Alphabet / Digit, add it to Postfix
+        elif x == "(":
+            stack.append(x)  # if x is "(" push to Stack
+        elif x == ")":  # if x is ")" pop stack until "(" is encountered
+            while stack[-1] != "(":
+                post_fix.append(stack.pop())  # Pop stack & add the content to Postfix
+            stack.pop()
+        else:
+            if len(stack) == 0:
+                stack.append(x)  # If stack is empty, push x to stack
+            else:  # while priority of x is not > priority of element in the stack
+                while len(stack) > 0 and priority[x] <= priority[stack[-1]]:
+                    post_fix.append(stack.pop())  # pop stack & add to Postfix
+                stack.append(x)  # push x to stack
+
+        print(
+            x.center(8),
+            ("".join(stack)).ljust(print_width),
+            ("".join(post_fix)).ljust(print_width),
+            sep=" | ",
+        )  # Output in tabular format
+
+    while len(stack) > 0:  # while stack is not empty
+        post_fix.append(stack.pop())  # pop stack & add to Postfix
+        print(
+            " ".center(8),
+            ("".join(stack)).ljust(print_width),
+            ("".join(post_fix)).ljust(print_width),
+            sep=" | ",
+        )  # Output in tabular format
+
+    return "".join(post_fix)  # return Postfix as str
+
+
+def infix_2_prefix(infix):
+    infix = list(infix[::-1])  # reverse the infix equation
+
+    for i in range(len(infix)):
+        if infix[i] == "(":
+            infix[i] = ")"  # change "(" to ")"
+        elif infix[i] == ")":
+            infix[i] = "("  # change ")" to "("
+
+    return (infix_2_postfix("".join(infix)))[
+        ::-1
+    ]  # call infix_2_postfix on Infix, return reverse of Postfix
+
+
+if __name__ == "__main__":
+    Infix = input("\nEnter an Infix Equation = ")  # Input an Infix equation
+    Infix = "".join(Infix.split())  # Remove spaces from the input
+    print("\n\t", Infix, "(Infix) -> ", infix_2_prefix(Infix), "(Prefix)")
